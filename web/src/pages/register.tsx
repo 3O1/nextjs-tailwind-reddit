1f30beb3756c94e89babb23801ac9b36a49f8a45
3O1 <jamesbrandonedwards301@gmail.com>
2021-08-15 05:45:58 -0400

    Added UI for register.tsx

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Register() {
   return (
@@ -13,6 +14,61 @@ export default function Register() {
         className="w-40 h-screen bg-center bg-cover"
         style={{ backgroundImage: "url('/img/bg.jpeg')" }}
       ></div>
+
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-70">
+          <h1 className="mb-2 text-lg font-medium">Sign up</h1>
+          <p className="mb-10 text-xs">
+            By continuing, you agree to our User Agreement and Privacy Policy
+          </p>
+
+          <form>
+            <div className="mb-6">
+              <input
+                type="checkbox"
+                className="mr-1 cursor-pointer"
+                id="agreement"
+              />
+              <label className="text-xs cursor-pointer" htmlFor="agreement">
+                I agree to get emails about cool stuff on Reddit
+              </label>
+            </div>
+
+            <div className="mb-2">
+              <input
+                type="email"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="mb-2">
+              <input
+                type="text"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Username"
+              />
+            </div>
+
+            <div className="mb-2">
+              <input
+                type="password"
+                className="w-full px-3 py-2 bg-gray-100 border border-gray-400 rounded"
+                placeholder="Password"
+              />
+            </div>
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded">
+              Sign up
+            </button>
+          </form>
+          <small>
+            Already a redditor?
+            <Link href="/login">
+              <a className="ml-1 text-blue-500 uppercase">Login</a>
+            </Link>
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: "17.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
